20c195a45d70d1a5f5a026413ba863771006b3ae
Austin Wiginton <devwig@devwigs-macbook.local>
2022-03-31 23:55:26 -0500

    Initial commit - picking back up on old project

---
+++ client/src/components/FeedbackForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import RatingSelect from './RatingSelect';
+import Button from './shared/Button';
+
+import Card from './shared/Card';
+
+function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+  
+  const handleTextChange = (e) => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(e.target.value);
+    
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} rating={rating}/>
+        <div className='input-group'>
+          <input value={text} onChange={handleTextChange} type='text' placeholder='Write a review' />
+          <Button type='submit' isDisabled={btnDisabled}>Submit</Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
