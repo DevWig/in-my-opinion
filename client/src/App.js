88635666b4890914a60d95df74830910e6d9ec1d
Austin Wiginton <devwig@devwigs-macbook.local>
2022-04-03 00:52:55 -0500

    Add functionality for adding new feedback, and add animation for adding and deleting feedback items

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -14,12 +15,17 @@ function App() {
     }
     // setFeedback = feedback.filter(id => id !== feedback.item.id);
   };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback])
+  }
   
   return (
     <>
     <Header /> 
     <div className="container">
-      <FeedbackForm />
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback} />
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
     </div>
--- client/src/components/FeedbackList.jsx
+++ client/src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import FeedbackItem from './FeedbackItem';
 
@@ -6,18 +7,40 @@ function FeedbackList({feedback, handleDelete}) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>
   }
-  
+
   return (
     <div>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+          
+        ))}
+      </AnimatePresence>
     </div>
   )
+  
+  // return (
+  //   <div>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList
