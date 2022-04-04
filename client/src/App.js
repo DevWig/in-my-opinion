07e5204a985ff26e2d6e14323ca421d6ad11a98e
Austin Wiginton <devwig@devwigs-macbook.local>
2022-04-03 21:56:31 -0500

    Add About page and add react-router-dom dependency for routing between different components; correct uuidv4 implementation in FeedbackData

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
+
 import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete this feedback?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-    // setFeedback = feedback.filter(id => id !== feedback.item.id);
+    // if (window.confirm('Are you sure you want to delete this feedback?')) {
+    //   setFeedback(feedback.filter((item) => item.id !== id))
+    // }
+    setFeedback(feedback.filter((item) => item.id !== id));
   };
 
   const addFeedback = (newFeedback) => {
@@ -22,14 +26,24 @@ function App() {
   }
   
   return (
-    <>
-    <Header /> 
-    <div className="container">
-      <FeedbackForm handleAdd={addFeedback}/>
-      <FeedbackStats feedback={feedback} />
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-    </div>
-    </>
+    <Router>
+      <Header /> 
+      <div className="container">
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }>
+            
+          </Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+      </div>
+      <AboutIconLink />
+    </Router>
     
   )
 };
--- client/src/data/FeedbackData.js
+++ client/src/data/FeedbackData.js
@@ -2,20 +2,20 @@ import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackData = [
   {
-    id: {uuidv4},
+    id: uuidv4(),
     rating: 10,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
-    id: {uuidv4},
+    id: uuidv4(),
     rating: 9,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
-    id: {uuidv4},
+    id: uuidv4(),
     rating: 8,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 ]
 
-export default FeedbackData
+export default FeedbackData;
